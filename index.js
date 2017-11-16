d8bc885e9a8b489836653b507a45406063318800
Chris Junker <dancetherapeutics@gmail.com>
2017-11-16 21:46:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 function scuberGreetingForFeet(distance) {
   if (distance<=400){
     return "This one is on me!"
-} else if ( distance < 2500 && distance > 2000){
-  return "I will gladly take your thirty bucks.";
-} else if ( distance > 2500){
-  return 'No can do.'
-}
+  } else if ( distance < 2500 && distance > 2000){
+    return "I will gladly take your thirty bucks.";
+  } else if ( distance > 2500){
+    return 'No can do.'
+  }
 }
 
 function ternaryCheckCity(city){
-  if ( city ==== 'NYC'){
-  return 'Ok, sounds good.'
-} else { 'No go.'
+  if (city ==== "NYC"){
+    return "Ok, sounds good."
+  } else { "No go."
+  }
 }
