c8420c5419e25c68f208ee1597c4ef61b7510891
Chris Junker <dancetherapeutics@gmail.com>
2017-11-16 21:00:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@
 function scuberGreetingForFeet(n) {
 ((n < 400) && (n === 400));
     return ("This one is on me!");
-  }if (n > 2000){
-    return ("I will gladly take your thirty bucks.");
-}}
+  }
