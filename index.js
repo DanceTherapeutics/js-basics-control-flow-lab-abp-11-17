d8daa19b923c2a02938bd63c18481bd3023e803d
Chris Junker <dancetherapeutics@gmail.com>
2017-11-16 20:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Write your code in this fil
 
 function scuberGreetingForFeet(n) {
-}  if {((n < 400) && (n === 400));
+((n < 400) && (n === 400));
     return ("This one is on me!");
-}else if { (n >= 2000) && (n <= 2501);
-    return ("I will gladly take your thirty bucks");
-}else if { ( n > 2500);
-    return ("No can do.")
 }
