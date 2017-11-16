452609bd6f48721493ff1a644df2119bc49a871a
Chris Junker <dancetherapeutics@gmail.com>
2017-11-16 21:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function scuberGreetingForFeet(distance) {
   return "I will gladly take your thirty bucks.";
 } else if ( distance > 2500){
   return 'No can do.'
+} 
 }
+function ternaryCheckCity(city){
+  if ( city ==== NYC)
+  return 'Ok, sounds good.'
 }
-
