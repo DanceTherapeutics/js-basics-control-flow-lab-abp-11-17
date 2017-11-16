15cac3bebc07495023c3cd1977ff0e98a4a6a544
Chris Junker <dancetherapeutics@gmail.com>
2017-11-16 21:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ function scuberGreetingForFeet(distance) {
   return 'No can do.'
 }
 }
+
 function ternaryCheckCity(city){
-  if ( city ==== 'NYC')
+  if ( city ==== 'NYC'){
   return 'Ok, sounds good.'
 } else { 'No go.'
 }
