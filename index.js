1c38b9c4e5006c3bf8dfbd3fc39774803228add3
Chris Junker <dancetherapeutics@gmail.com>
2017-11-16 21:20:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Write your code in this fil
 
-let scuberGreetingForFeet(n) = scuberGreetingForFeet(199) {
-((n < 400) && (n === 400));
+function scuberGreetingForFeet(n) {
+if (n<400);
     return ("This one is on me!");
-  }else{ (n >= 2000)
-  return ("I will gladly take your thirty bucks")
 }
